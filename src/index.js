50192d5012fb7dcdce9c109f59751fad1d614366
Jeremy Hart <jeremynhart@gmail.com>
2022-08-03 00:49:38 -0500

    CWV Link Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Metrics } from '@layer0/rum';
 import App from './App';
+
+new Metrics({
+  token: '17a1a83e-d3bf-4893-9a0c-e78eaa33a1d3',
+}).collect();
 
 ReactDOM.render(<App />, document.getElementById('root'));
